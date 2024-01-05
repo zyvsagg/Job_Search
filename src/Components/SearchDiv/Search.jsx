a3b611807b0ed8edb02e97652512a5f766e6dd4d
zyvsagg <iamskriller@gmail.com>
2024-01-05 13:04:52 +0545

    Search section done

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import {AiOutlineSearch} from 'react-icons/ai'
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { BsHouseDoor } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
 
 const Search = () => {
   return (
@@ -8,16 +10,96 @@ const Search = () => {
       <form action="">
         <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
           <div className="flex gap-2 items-center">
-          <AiOutlineSearch className='text-[25px] icon' />
-          <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search Job Here...'/>
-          <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6]
-          hover:text-textColor icon'/>
-         
-         
+            <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search Job Here..."
+            />
+            <AiOutlineCloseCircle
+              className="text-[30px] text-[#a5a6a6]
+          hover:text-textColor icon"
+            />
           </div>
-          
+        
+
+          <div className="flex gap-2 items-center">
+            <BsHouseDoor className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search by Company..."
+            />
+            <AiOutlineCloseCircle
+              className="text-[30px] text-[#a5a6a6]
+          hover:text-textColor icon"
+            />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <CiLocationOn className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search by Location..."
+            />
+            <AiOutlineCloseCircle
+              className="text-[30px] text-[#a5a6a6]
+          hover:text-textColor icon"
+            />
+          </div>
+          <button className="bg-blueColor h-full p-5 px-10 rounded-[10px]
+          text-white cursor-pointer hover:bg-blue-300">Search</button>
         </div>
       </form>
+
+    <div className="secDiv flex items-center gap-10 justify-center mt-5">
+
+        <div className="singleSearch flex items-center gap-2 ">
+            <label htmlFor="type" className="text-[#808080] font-semibold">Type :</label>
+
+            <select name="" id="relevance" className="bg-white rounded-[3px] px-4 py-1">
+            <option value="">Full-Time</option>
+            <option value="">Part-Time</option>
+            <option value="">Intern</option>
+            <option value="">Remote</option>
+            </select>
+            
+        </div>
+
+        
+        <div className="singleSearch flex items-center gap-2">
+            <label htmlFor="level" className="text-[#808080] font-semibold">Level :</label>
+
+            <select name="" id="level" className="bg-white rounded-[3px] px-4 py-1">
+            <option value="">Senior</option>
+            <option value="">Beginner</option>
+            <option value=""> Intermediate</option>
+            <option value="">Advance</option>
+            </select>
+            
+        </div>
+
+
+        <div className="singleSearch flex items-center gap-2">
+            <label htmlFor="relevance" className="text-[#808080] font-semibold">Sort By :</label>
+
+            <select name="" id="relevanc" className="bg-white rounded-[3px] px-4 py-1">
+            <option value="">Relevance</option>
+            <option value="">Inclusive</option>
+            <option value="">Starts With</option>
+            <option value="">Contains</option>
+            </select>
+            
+        </div>
+    
+            <span className="text-[#808080] cursor-pointer  ">Clear All</span>
+        
+
+    </div>
+
+
+
     </div>
   );
 };
