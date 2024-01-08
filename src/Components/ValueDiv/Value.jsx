f2414c796406d77b51db5a2d4ff5bf49db09b8c4
zyvsagg <iamskriller@gmail.com>
2024-01-08 06:04:53 +0545

    values

--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+
+import logo from "../../assets/logo.png";
 
 const Value = () => {
   return (
-    <div>Value</div>
-  )
-}
+    <div className="mb-[4rem] mt-[6rem]">
+      <h1
+        className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-boldw-[400px block]">
+        The value that holds us true and to account
+      </h1>
+      <div className="grid gap-[10rem] grid-cols-3 items-center">
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex item-center gap-3">
+            <div
+              className=" imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[48px]
+            w-[40px] flex items-center justify-center "
+            >
+              <img src={logo} alt=" something like bird" className=" w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p
+            className="text-[25px] text-textColor opacity-[.7] py-[1rem]
+          fontsize-semibold"
+          >
+            The greatest glory in living lies not in never falling, but in
+            rising every time we fall.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Value
+export default Value;
